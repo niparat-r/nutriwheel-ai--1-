cfc92eefe9e81f2f30fde920c9f18d95cb329b56
niparat-r <niparat.r@sk.ac.th>
2025-12-02 21:03:32 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,11 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  base: '/nutriwheel-ai--1-/',
   // Use '.' as the environment directory to avoid TypeScript errors with process.cwd()
-  //const env = loadEnv(mode, '.', '');
-  const apiKey = import.meta.env.VITE_API_KEY;
+  const env = loadEnv(mode, '.', '');
   return {
     plugins: [react()],
-
     define: {
-
       // Allows usage of process.env.API_KEY in the browser for this specific app structure
       'process.env.API_KEY': JSON.stringify(env.API_KEY)
     }
