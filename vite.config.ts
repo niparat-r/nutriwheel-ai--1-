b889380c1de55f595255c6d6b461dda4c5c39654
niparat-r <niparat.r@sk.ac.th>
2025-12-02 20:54:02 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
+  base: '/nutriwheel-ai--1-/',
   // Use '.' as the environment directory to avoid TypeScript errors with process.cwd()
   const env = loadEnv(mode, '.', '');
   return {
